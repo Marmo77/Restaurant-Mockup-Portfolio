3509e4b06b411a7cb545cf61552e88ffe11ca3ba
Marmo77 <matyskamela@gmail.com>
2026-06-09 13:05:49 +0200

    dodanie menu zdjęć (własne) do Menu

--- src/data/menuData.ts
+++ src/data/menuData.ts
@@ -9,24 +9,24 @@ export const menuData: MenuCategory[] = [
     id: "burgers",
     label: "Burgery",
     menuImages: [
-      "https://cdn.website.dish.co/media/aa/fa/9394848/Menu-burger.jpg",
-      "https://cdn.website.dish.co/media/15/e8/9394871/Menu-burgers-EN.jpg",
+      "/menu/burgery1.jpg",
+      "/menu/burgery2.jpg",
     ],
   },
   {
     id: "lunch",
     label: "Lunch",
     menuImages: [
-      "https://cdn.website.dish.co/media/f0/db/9394879/Menu-lunch.jpg",
-      "https://cdn.website.dish.co/media/48/ca/9394872/Menu-lunch-EN.jpg",
+      "/menu/lunch1.jpg",
+      "/menu/lunch2.jpg",
     ],
   },
   {
     id: "drinks",
     label: "Napoje & Kraft",
     menuImages: [
-      "https://cdn.website.dish.co/media/18/68/9394854/Menu-pynne.jpg",
-      "https://cdn.website.dish.co/media/9b/25/9394860/Menu-koktajle.jpg",
+      "/menu/napoje1.jpg",
+      "/menu/napoje2.jpg",
     ],
   },
 ];
